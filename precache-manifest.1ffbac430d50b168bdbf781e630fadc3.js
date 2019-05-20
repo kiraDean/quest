62e3414ff8f69c037538bd643aa2c6db9f4dacbd
kira s <kira.totti97@gmail.com>
2019-05-20 15:02:44 +0300

    Updates

---
+++ precache-manifest.1ffbac430d50b168bdbf781e630fadc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0684047351b890aaef955df96d57465",
+    "url": "/quest/index.html"
+  },
+  {
+    "revision": "3e52b3fe7611bdc3e66d",
+    "url": "/quest/static/css/main.9914b1f4.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/quest/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "3e52b3fe7611bdc3e66d",
+    "url": "/quest/static/js/main.6a25bc74.chunk.js"
+  },
+  {
+    "revision": "0eb63cc19cb68c571357",
+    "url": "/quest/static/js/runtime~main.0297c361.js"
+  },
+  {
+    "revision": "474c07db294f528c2cc9ae69de5bf21a",
+    "url": "/quest/static/media/2.474c07db.jpg"
+  }
+]);
